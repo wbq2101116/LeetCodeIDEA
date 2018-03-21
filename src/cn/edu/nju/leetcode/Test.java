24f15c51e95ce4b6c13379d7797886fc61b92922
wangbingquan <773854936@qq.com>
2018-03-21 11:19:29 +0800

    initial 2
    unknown

--- src/cn/edu/nju/leetcode/Test.java
+++ src/cn/edu/nju/leetcode/Test.java
@@ -27,14 +27,18 @@ public class Test {
 }
 
 /*
+// 621 Task Scheduler
 class Solution {
+
     public int smallestDistancePair(int[] nums, int k) {
         Arrays.sort(nums);
         int n = nums.length;
         int[] res = new int[nums[n - 1] - nums[0] + 1];
+        // int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 res[nums[j] - nums[i]]++;
+            }
         }
         // Arrays.sort(res);
         int count = 0;
